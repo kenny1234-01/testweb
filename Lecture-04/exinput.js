8d80c525815c9705f146203d6ecf214ecf71dc8a
kenny1234-01 <s6406021610046@email.kmutnb.ac.th>
2025-01-14 08:51:43 +0700

    save

---
+++ Lecture-04/exinput.js
@@ -0,0 +1,12 @@
+const rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter your name: ', (name) => {
+    rl.question('Enter your weight (in kg): ', (weight) => {
+        rl.question('Enter your height (in m): ', (weight) => {
+            
+        });
+    });
+});``
--- Lecture-05/promise.js
+++ Lecture-05/promise.js
@@ -8,6 +8,14 @@ console.log('Start');
 promiseTimeout(2000)
     .then(()  => {
         console.log("Done!!") ;
+        return promiseTimeout(1000);
+    })
+    .then(()  => {
+        console.log("Also done!!") ;
+        return Promise.resolve(42);
+    })
+    .then((result)  => {
+        console.log(result) ;
     })
     .catch(() => {
         console.log("Error!");
