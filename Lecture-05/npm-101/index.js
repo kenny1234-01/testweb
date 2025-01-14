b950a49a4d1153e8b0d0eb0d6f3df85c35541a71
kenny1234-01 <s6406021610046@email.kmutnb.ac.th>
2025-01-14 09:58:55 +0700

    save

--- Lecture-05/npm-101/index.js
+++ Lecture-05/npm-101/index.js
@@ -1,12 +1,13 @@
-//require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
